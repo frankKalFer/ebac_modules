7fa5f55afc3de06eacba875ba9cfa36f2805437f
frankKalFer <franklinfernnades22@gmail.com>
2024-09-02 07:59:49 +0200

    components

--- mod_28_react/src/App.jsx
+++ mod_28_react/src/App.jsx
@@ -1,21 +1,11 @@
+import Perfil from "./components/Perfil";
+
+
+
 function App() {
-  const nome = "Franklin"
-
-  function retornaNome() {
-    return nome
-  }
-
-  const pessoa = {
-    nome: "Beci"
-  }
-
-  let estaDeDia = true;
-
   return (
     <>
-      <h1>Ola, {pessoa.nome}</h1>
-      <h2>Fernandes</h2>
-      {estaDeDia ? 'Bom dia' : "Boa tarde"}
+      <Perfil />
     </>
 
   )
---
+++ mod_28_react/src/components/Perfil/index.jsx
@@ -0,0 +1,17 @@
+import './perfil.css'
+
+const Perfil = () => {
+
+    const usuario = {
+        nome: 'Franklin Kalinowski',
+        avatar: 'https://github.com/frankKalFer.png'
+    }
+    return (
+        <div>
+            <img className="perfil-avatar" src={usuario.avatar} />
+            <h3 className='perfil-titulo'>{usuario.nome}</h3>
+        </div>
+    )
+}
+
+export default Perfil;
