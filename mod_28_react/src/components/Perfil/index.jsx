dc7b6ea0979a1c0b0605a306cc60eb4a13a946d5
frankKalFer <franklinfernnades22@gmail.com>
2024-09-02 08:00:21 +0200

    components

--- mod_28_react/src/components/Perfil/index.jsx
+++ mod_28_react/src/components/Perfil/index.jsx
@@ -1,11 +1,11 @@
 import './perfil.css'
 
 const Perfil = () => {
-
     const usuario = {
         nome: 'Franklin Kalinowski',
         avatar: 'https://github.com/frankKalFer.png'
     }
+
     return (
         <div>
             <img className="perfil-avatar" src={usuario.avatar} />
