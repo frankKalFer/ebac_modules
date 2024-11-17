fcfb479bbb38f5a22d59d43de387da96ef2f779c
frankKalFer <franklinfernnades22@gmail.com>
2024-11-17 22:26:00 +0100

    configurando syled components

---
+++ mod_29_CSS_JS_react/src/teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: blue;
+  color: #fff;
+`
+function Teste() {
+  return <Botao>Clique aqui</Botao>
+}
+
+export default Teste
