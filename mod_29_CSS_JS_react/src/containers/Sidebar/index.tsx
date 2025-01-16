5b07f56184731ca86c3a985a43fd2f2bc0d1b811
frankKalFer <franklinfernnades22@gmail.com>
2025-01-16 05:42:13 +0100

    construcao da estrutura e secao sobre mim

---
+++ mod_29_CSS_JS_react/src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/frankKalFer.png" alt="" />
+    <Titulo fontSize={20}>Franklin Kalinowski</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ mod_29_CSS_JS_react/src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique illo
+      doloremque quidem tenetur expedita voluptas eveniet animi inventore facere
+      sequi? Blanditiis excepturi ut aliquam similique voluptate eius aliquid
+      nisi molestias.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=frankKalFer&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=frankKalFer&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
